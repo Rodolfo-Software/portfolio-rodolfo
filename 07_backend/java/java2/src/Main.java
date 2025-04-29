500542b59bca574d4cd898b3bb78a9c8569de070
root <root@Satan.>
2025-04-29 11:25:36 -0600

    java 2

---
+++ 07_backend/java/java2/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Crear un objeto de la clase Perro
+        Perro miPerro = new Perro();
+
+        // Darle valores al objeto
+        miPerro.nombre = "Fido";
+        miPerro.edad = 3;
+
+        // Llamar al método ladrar
+        miPerro.ladrar();
+    }
+}
---
+++ 07_backend/java/java2/src/Perro.java
@@ -0,0 +1,11 @@
+public class Perro {
+    // Atributos (características del perro)
+    String nombre;
+    int edad;
+
+    // Método (acción del perro)
+    void ladrar() {
+        System.out.println(nombre + " dice: ¡Guau guau!");
+    }
+}
+
